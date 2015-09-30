56eb38a09b4d2e92543be6e47e96cbf6dd6eadd8
brettanomyces <brettyukich@gmail.com>
2015-10-01 10:54:14 +1300

    add timestamp to axel output

--- axel.sh
+++ axel.sh
@@ -3,6 +3,6 @@
 while true
 do
     ./dumas.sh
-    echo "dumas stopped with exit code $?.  Respawning.." >&2 
+    echo "dumas stopped with exit code $?.  Respawning.." | ts >&2 
     sleep 1
 done
