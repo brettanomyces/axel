0c49249b89fd31968bd0766b2a53be9828fd20ce
brettanomyces <brettyukich@gmail.com>
2015-10-05 14:37:58 +1300

    conslidate scripts into one

--- axel.sh
+++ axel.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-while true
-do
-    cat /dev/ttyACM0 | sed -u '/^\s*$/d' | ./log.sh
-    echo "serial connection broken, reconnecting.." | ts >&2
-    sleep 1
+function log {
+  while read line; do
+    day=$(date "+%F")
+    time=$(date "+%T")
+    echo "$time","$line" >> log/"$day"_hermes.log
+  done
+}
+
+while true; do
+  cat /dev/ttyACM0 | sed -u '/^\s*$/d' | log
+  echo "serial connection broken, reconnecting.." | ts >&2
+  sleep 1
 done
--- log.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-while read line; do
-  day=$(date "+%F")
-  time=$(date "+%T")
-  echo "$time","$line" >> log/"$day"_hermes.log
-done
