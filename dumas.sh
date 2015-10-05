263edb75e5d87dd51fbcfe6a5d702563372d99f7
brettanomyces <brettyukich@gmail.com>
2015-10-05 14:32:35 +1300

    remove redundant script

--- dumas.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# sed -u '/^\s*$/d': delete empty lines
-# ts: add timestamp
-#cat /dev/ttyACM0 | sed -u '/^\s*$/d' | ts >> hermes.log
-cat /dev/ttyACM0 | sed -u '/^\s*$/d' | ./log.sh
-exit 1
